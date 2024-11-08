1ccf6c79745f4ca56b95a13fab06a75e0ca2011d
Aaron-AA0721 <aaronaa8231@gmail.com>
2024-11-08 13:05:12 +0000

    fix compile bugs and add testing code

--- test/test.ino
+++ test/test.ino
@@ -7,10 +7,11 @@
 #define MAX_RANG (520)
 #define ADC_SOLUTION (1023.0)
 DFRobot_VL53L0X ToFSensor;
+Servo myservo;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1); // left Motor on port M1
-Adafruit_DCMotor *RightMotor = AFMS.getMotor(2); // right Motor on port M2
-Adafruit_DCMotor *exMotor = AFMS.getMotor(3); // extra 18RPM motor
+Adafruit_DCMotor *RightMotor = AFMS.getMotor(3); // right Motor on port M2
+Adafruit_DCMotor *exMotor = AFMS.getMotor(2); // extra 18RPM motor
 
 int RLEDPin = 11; // the output pin for the Red LED
 int GLEDPin = 12; // the output pin for the Green LED
@@ -40,27 +41,57 @@ void setup() {
   pinMode(RightLineSensorPin, INPUT);
   pinMode(FrontLineSensorPin, INPUT);
 
-  myservo.attach(ServoPin)
+  myservo.attach(ServoPin);
   if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
     Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
-  }
+  }}
   Serial.println("Motor Shield found.");
+  exMotor->setSpeed(150);
+  exMotor->run(FORWARD);
+  exMotor->run(RELEASE);
 }
 float UltraRead,UltraDistance;
-float TofDistance;
+float ToFDistance;
 int LeftLineRead,RightLineRead,FrontLineRead;
 int MagRead = 0; // variable for reading the pin status
 void loop(){
-  MagRead = digitalRead(inputPin); // read input value
+  MagRead = digitalRead(MagneticPin); // read input value
   UltraRead = analogRead(UltrasonicPin);
   UltraDistance = UltraRead * MAX_RANG / ADC_SOLUTION;
-  TofDistance = TofSensor.getDistance();
+  
+  //Serial.println(UltraDistance,0);
+  //Serial.println("cm");
+  ToFDistance = ToFSensor.getDistance();
   LeftLineRead =  digitalRead(LeftLineSensorPin);
   RightLineRead =  digitalRead(RightLineSensorPin);
   FrontLineRead =  digitalRead(FrontLineSensorPin);
+  digitalWrite(RLEDPin,HIGH);
+  //Serial.println(FrontLineRead);
+  delay(500);
+  move();
 }
 void move(){
- //?
+  exMotor->run(FORWARD);
+  for (int i=0; i<255; i++) {
+    exMotor->setSpeed(i);
+    delay(10);
+  }
+  for (int i=255; i!=0; i--) {
+    exMotor->setSpeed(i);
+    delay(10);
+  }
+
+  Serial.print("tock");
+
+  exMotor->run(BACKWARD);
+  for (int i=0; i<255; i++) {
+    exMotor->setSpeed(i);
+    delay(10);
+  }
+  for (int i=255; i!=0; i--) {
+    exMotor->setSpeed(i);
+    delay(10);
+  }
 }
