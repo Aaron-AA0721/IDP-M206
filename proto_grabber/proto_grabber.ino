a653948396e2b8000c958f2e03aa435166773f7b
Sophia-Gilbert <sg2112@cam.ac.uk>
2024-11-21 10:32:17 +0000

    Added magnetic sensor and LEDs

--- proto_grabber/proto_grabber.ino
+++ proto_grabber/proto_grabber.ino
@@ -5,25 +5,29 @@ Servo myservo2; // lifter
 
 int pos1 = 10; // lifter starting angle
 int pos2 = 90; // grabber starting angle
-int crashswitchPin = 7;
-int magPin = 9;
+int crashswitchPin = 7;  // the input pin for the switch
+int magPin = 9;  // the input pin for the magnetic sensor
+int RLEDPin = 11; // the output pin for the Red LED
+int GLEDPin = 12; // the output pin for the Green LED
 
 void setup() {
-  myservo1.attach(12); 
+  myservo1.attach(10); 
   myservo2.attach(4);
   Serial.begin(9600); 
   pinMode(crashswitchPin, INPUT);
+  pinMode(magPin, INPUT);
   myservo1.write(pos1); // possibly not needed
   myservo2.write(pos2);
 }
 
 void loop() {
-  int val = digitalRead(crashswitchPin); // read input value
+  int box-held = digitalRead(crashswitchPin); // read crash switch value
+  int mag = digitalRead(magPin); // read magnetic sensor
 
-if ((pos2 > 0) && (val == LOW)) { // lifts box 90 degrees up
+  if ((pos2 > 0) && (box-held == LOW)) { // lifts box 90 degrees up
     pos2 -= 1;
     myservo2.write(pos2);
-    delay(15); 
+    delay(15);
   }
 
   if (pos1 < 100) { // grabber moves from 10 to 100 degrees
@@ -32,7 +36,17 @@ if ((pos2 > 0) && (val == LOW)) { // lifts box 90 degrees up
     delay(30); // could be made faster
   }
 
-  if (val == LOW) {
-    Serial.println("Box grabbed"); // or move on to next action
+  // turns on the light when the box is being lifted
+  if ((89 >pos2 > 1) && (mag == LOW)) { // box is magnetic so light is green
+    GLED == HIGH
+    RLED == LOW
+  }
+  elif ((89 >pos2 > 1) && (mag == HIGH)) { // box isn't magnetic so light is red
+    GLED == LOW
+    RLED == HIGH
+  }
+  else { // lights are off when the box isn't being lifted
+    GLED == LOW
+    RLED == LOW
   }
 }
